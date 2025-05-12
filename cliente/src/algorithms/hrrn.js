2c35cef3aeb54b2c0749c82cf5bbb5812f5867c7
Cesar Del Rio <cesardelriodlr@outlook.com>
2025-05-12 05:06:50 -0600

    xd

--- cliente/src/algorithms/hrrn.js
+++ cliente/src/algorithms/hrrn.js
@@ -2,11 +2,8 @@
 // Este algoritmo selecciona el proceso con el mejor ratio de respuesta (RR)
 // Si hay empate, selecciona el primero en llegar
 
-
 class HRRNScheduler {
- 
   constructor(processes) {
-   
     this.processes = processes.map((p) => ({
       pid: p.pid,
       arrival: p.arrival,
@@ -16,21 +13,18 @@ class HRRNScheduler {
       turnaround: 0,
       completion: 0,
       finished: false,
-      responseTime: -1, 
-      weightedTAT: 0,  
+      responseTime: -1,
+      weightedTAT: 0,
     }));
 
-  
     this.clock = Math.min(...this.processes.map((p) => p.arrival));
     this.gantt = []; // Historial para diagrama de Gantt
   }
 
- 
   _readyQueue() {
     return this.processes.filter((p) => !p.finished && p.arrival <= this.clock);
   }
 
-  
   _pickProcess(ready) {
     return ready.sort((a, b) => {
       const rrA = (this.clock - a.arrival + a.burst) / a.burst;
@@ -41,12 +35,10 @@ class HRRNScheduler {
     })[0];
   }
 
- 
   run() {
     while (this.processes.some((p) => !p.finished)) {
       const ready = this._readyQueue();
 
-     
       if (ready.length === 0) {
         const nextArrival = Math.min(
           ...this.processes.filter((p) => !p.finished).map((p) => p.arrival)
@@ -55,15 +47,14 @@ class HRRNScheduler {
         continue;
       }
 
-      
       const current = this._pickProcess(ready);
       const start = this.clock;
-      
-    // tiempo de respuesta 
+
+      // tiempo de respuesta
       if (current.responseTime === -1) {
         current.responseTime = start - current.arrival;
       }
-      
+
       this.clock += current.burst;
       const finish = this.clock;
 
@@ -73,22 +64,22 @@ class HRRNScheduler {
       current.waiting = current.turnaround - current.burst;
       current.weightedTAT = current.turnaround / current.burst;
 
-     
       this.gantt.push({ pid: current.pid, start, finish });
     }
   }
 
- 
   getResults() {
     const avgWaiting =
       this.processes.reduce((s, p) => s + p.waiting, 0) / this.processes.length;
     const avgTurnaround =
       this.processes.reduce((s, p) => s + p.turnaround, 0) /
       this.processes.length;
-    const avgWeightedTat = 
-      this.processes.reduce((s, p) => s + p.weightedTAT, 0) / this.processes.length;
-    const avgResponseTime = 
-      this.processes.reduce((s, p) => s + p.responseTime, 0) / this.processes.length;
+    const avgWeightedTat =
+      this.processes.reduce((s, p) => s + p.weightedTAT, 0) /
+      this.processes.length;
+    const avgResponseTime =
+      this.processes.reduce((s, p) => s + p.responseTime, 0) /
+      this.processes.length;
 
     return {
       processes: this.processes.map((p) => ({
@@ -99,17 +90,16 @@ class HRRNScheduler {
         Retorno: p.turnaround,
         Finalizacion: p.completion,
         weightedTAT: p.weightedTAT,
-        response: p.responseTime
+        response: p.responseTime,
       })),
       gantt: this.gantt,
       avgWT: avgWaiting,
       avgTAT: avgTurnaround,
       avgWeightedTAT: avgWeightedTat,
-      avgResponse: avgResponseTime
+      avgResponse: avgResponseTime,
     };
   }
 }
-
 
 if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
   module.exports = HRRNScheduler;
